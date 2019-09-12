105c3ce574e953904e907660c84a13e5a4d2015f
Julius <juliusblusevicius20@gmail.com>
2019-09-12 14:54:13 +0300

    Using the ParametrizedThreadStart

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -6,22 +6,20 @@ namespace Chapter1
     public static class Program
     {
 
-        public static void ThreadMethod()
+        public static void ThreadMethod(object o)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < (int)o; i++)
             {
                 Console.WriteLine("ThreadProc: {0}", i);
-                Thread.Sleep(1000);
+                Thread.Sleep(0);
             }
         }
 
         public static void Main()
         {
-            Thread t = new Thread(new ThreadStart(ThreadMethod))
-            {
-                IsBackground = true
-            };
-            t.Start();
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
         }
     }
 }

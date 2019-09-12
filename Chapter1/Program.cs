634146ba0f9601dd6c3a488a116d39d0cea88b18
Julius <juliusblusevicius20@gmail.com>
2019-09-12 14:57:47 +0300

    Stopping a thread

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -17,9 +17,24 @@ namespace Chapter1
 
         public static void Main()
         {
-            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
-            t.Start(5);
+            bool stopped = false;
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+
+            t.Start();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            stopped = true;
             t.Join();
+
+
         }
     }
 }

89790dcd3ccaf6f08f56b773356a528d208565cc
Julius <juliusblusevicius20@gmail.com>
2019-09-16 14:28:34 +0300

    Ordered parallel query

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -10,7 +10,7 @@ namespace Chapter1
         public static void Main()
         {
             var numbers = Enumerable.Range(0, 10);
-            var parallelResult = numbers.AsParallel()
+            var parallelResult = numbers.AsParallel().AsOrdered()
                 .Where(i => i % 2 == 0)
                 .ToArray();
 

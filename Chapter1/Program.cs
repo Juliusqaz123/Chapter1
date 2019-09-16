d570fc53ee53ba2d4718b3f381472c346a510ce6
Julius <juliusblusevicius20@gmail.com>
2019-09-16 13:32:39 +0300

    Adding a continuation

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -11,8 +11,12 @@ namespace Chapter1
             Task<int> t = Task.Run(() =>
             {
                 return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
             });
-            Console.WriteLine(t.Result); // Displays 42
+
+            Console.WriteLine(t.Result); // Displays 84
         }
     }
 }

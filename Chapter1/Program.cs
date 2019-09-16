d93005b7d6cf641a3ae0d36c3cdf58e6de5c0b06
Julius <juliusblusevicius20@gmail.com>
2019-09-16 15:21:27 +0300

    Using a ConcurrentDictionary

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -10,12 +10,21 @@ namespace Chapter1
 
         public static void Main()
         {
-            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
-            queue.Enqueue(42);
+            var dict = new ConcurrentDictionary<string, int>();
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
 
-            int result;
-            if (queue.TryDequeue(out result))
-                Console.WriteLine("Dequeud: {0}", result);
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+
+            dict["k1"] = 42; // Overwrite unconditionally
+
+            int r1 = dict.AddOrUpdate("k1", 3, (s, i) => i * 2);
+            int r2 = dict.GetOrAdd("k2", 3);
         }
 
     }

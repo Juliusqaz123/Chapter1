700454f204f7386d69b66039fe6319460570a973
Julius <juliusblusevicius20@gmail.com>
2019-09-16 14:32:13 +0300

    Using ForAll

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -11,12 +11,10 @@ namespace Chapter1
         {
             var numbers = Enumerable.Range(0, 20);
 
-            var parallelResult = numbers.AsParallel().AsOrdered()
-                .Where(i => i % 2 == 0).AsSequential();
+            var parallelResult = numbers.AsParallel()
+                .Where(i => i % 2 == 0);
 
-            foreach (int i in parallelResult.Take(5))
-                Console.WriteLine(i);
-
+            parallelResult.ForAll(e => Console.WriteLine(e));
         }
     }
 }

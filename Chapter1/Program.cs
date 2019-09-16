ca2228fdc49c200abb5efa2406aac271e8610a9c
Julius <juliusblusevicius20@gmail.com>
2019-09-16 15:16:52 +0300

    Using a ConcurrentStack

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -10,18 +10,23 @@ namespace Chapter1
 
         public static void Main()
         {
-            ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            Task.Run(() =>
-            {
-                bag.Add(42);
-                Thread.Sleep(1000);
-                bag.Add(21);
-            });
-            Task.Run(() =>
-            {
-                foreach (int i in bag)
-                    Console.WriteLine(i);
-            }).Wait();
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+
+            stack.Push(42);
+
+            int result;
+            if (stack.TryPop(out result))
+                Console.WriteLine("Popped: {0}", result);
+
+            stack.PushRange(new int[] { 1, 2, 3 });
+
+            int[] values = new int[2];
+            stack.TryPopRange(values);
+
+            foreach (int i in values)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
         }
 
     }

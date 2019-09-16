4a448196218cc97ae93a38dcd969737138298d30
Julius <juliusblusevicius20@gmail.com>
2019-09-16 13:58:38 +0300

    Using Parallel.Break

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,16 +10,16 @@ namespace Chapter1
 
         public static void Main()
         {
-            Parallel.For(0, 10, i =>
-            {
-                Thread.Sleep(1000);
-            });
+            Parallel.
+                For(0, 1000, (int i, ParallelLoopState loopState) =>
+                {
+                    if (i == 500)
+                    {
+                        Console.WriteLine("Breaking loop");
+                        loopState.Break();
+                    }
 
-            var numbers = Enumerable.Range(0, 10);
-            Parallel.ForEach(numbers, i =>
-            {
-                Thread.Sleep(1000);
-            });
+                });
         }
     }
 }

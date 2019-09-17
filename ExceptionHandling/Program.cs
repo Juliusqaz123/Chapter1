b2ba66710e01ae5db866a38eaf651fc723ea3a69
Julius <juliusblusevicius20@gmail.com>
2019-09-17 16:16:08 +0300

    Using a finally block

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -5,23 +5,23 @@ namespace ExceptionHandling
     {
         public static void Main(string[] args)
         {
-            while (true)
+            string s = Console.ReadLine();
+
+            try
             {
-                string s = Console.ReadLine();
-
-                try
-                {
-                    int i = int.Parse(s);
-                }
-                catch (ArgumentNullException)
-                {
-                    Console.WriteLine("You need to enter a value");
-                }
-
-                catch (FormatException)
-                {
-                    Console.WriteLine("{0} is not a valid number. Please try again", s);
-                }
+                int i = int.Parse(s);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You need to enter a value");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not a valid number. Please try again", s);
+            }
+            finally
+            {
+                Console.WriteLine("Program complete.");
             }
         }
     }

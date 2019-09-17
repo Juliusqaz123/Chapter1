54d429b9129f90b91e0a1c78d66d2cdedae91441
Julius <juliusblusevicius20@gmail.com>
2019-09-17 15:18:26 +0300

    Catching a FormatException

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,11 +1,26 @@
-﻿namespace ExceptionHandling
+﻿using System;
+namespace ExceptionHandling
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            string s = "NaN";
-            int i = int.Parse(s);
+            while (true)
+            {
+                string s = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(s)) break;
+
+                try
+                {
+                    int i = int.Parse(s);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} is not a valid number. Please try again", s);
+                }
+            }
         }
     }
 }

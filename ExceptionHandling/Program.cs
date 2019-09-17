e63d09028d496be369ae60c978150e49f154c6fa
Julius <juliusblusevicius20@gmail.com>
2019-09-17 15:16:15 +0300

    Parsing an invalid number

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExceptionHandling
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "NaN";
+            int i = int.Parse(s);
+        }
+    }
+}

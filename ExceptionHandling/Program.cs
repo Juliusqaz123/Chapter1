9570a89aa86f74a9c28f869d5158018dcd805c2a
Julius <juliusblusevicius20@gmail.com>
2019-09-17 16:18:01 +0300

    Using Environment.FailFast

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -6,22 +6,15 @@ namespace ExceptionHandling
         public static void Main(string[] args)
         {
             string s = Console.ReadLine();
-
+            
             try
             {
                 int i = int.Parse(s);
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("You need to enter a value");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("{0} is not a valid number. Please try again", s);
+                if (i == 42) Environment.FailFast("Special number entered");
             }
             finally
             {
-                Console.WriteLine("Program complete.");
+                Console.WriteLine("Program complete");
             }
         }
     }
